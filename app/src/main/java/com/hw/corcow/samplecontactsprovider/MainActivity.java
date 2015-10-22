64cd25c7d4c017a11700758ce25701f3e9326bfa
= <cho7673@gmail.com>
2015-10-22 14:29:54 +0900

    Loader 추가

--- app/src/main/java/com/hw/corcow/samplecontactsprovider/MainActivity.java
+++ app/src/main/java/com/hw/corcow/samplecontactsprovider/MainActivity.java
@@ -4,6 +4,9 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -15,7 +18,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     ListView listView;
     EditText keywordView;
@@ -51,7 +54,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String keyword = s.toString();
-                getContacts(keyword);
+                // getContacts(keyword);
+                Bundle args = new Bundle();
+                args.putString("keyword", keyword);
+                getSupportLoaderManager().restartLoader(0, args, MainActivity.this);
             }
 
             @Override
@@ -59,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        getSupportLoaderManager().initLoader(0, null, this);   // 첫번째인자 : Loader의 id코드(0) / 두번째인자 : 내가 LoaderManager에게 전달하는 argument
+        // >> onCreateLoader가 바로 호출됨. ( initLoader의 1,2번째 인자를 넘겨준다)
 
     }
 
@@ -71,10 +80,11 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.changeCursor(c);
     }
 
+    /* Loader로 처리했으므로 불필요
     @Override
     protected void onResume() {
         super.onResume();
-        getContacts(null);
+        // getContacts(null);
     }
 
     @Override
@@ -82,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         mAdapter.changeCursor(null);
     }
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -103,5 +114,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Loader
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+
+        if (args != null) {
+            String keyword = args.getString("keyword");
+            if(!TextUtils.isEmpty(keyword)) {
+                uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, Uri.encode(keyword));
+            }
+        }
+        return new CursorLoader(this, uri, projection, selection, null, sortOrder);         // LoaderManager가 얻은 데이터가 이것이라고 말해줌
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {            // 획득한 데이터를 알려줌
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {                          // loader가 유효하지 않다면
+        mAdapter.swapCursor(null);
+        /** 결과를 release 하는 것을 Loader가 내부적으로 처리
+         * Activity가 finish되면 >> 이전엔 Cursor를 close 해주어야했음.
+         * Activity가 finish되면, onLoaderReset 가 호출되면서 내부적으로 처리해줌
+         */
     }
 }
